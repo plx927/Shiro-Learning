539460f4135ca40f24c351f7acfbd8cb40b6894d
plx927 <784580872@qq.com>
2016-09-02 20:43:44 +0800

    完成Shrio WebApp例子的学习。

--- shrio-learning-helloworld/src/main/java/com/panlingxiao/shiro/helloworld/HelloWorld.java
+++ shrio-learning-helloworld/src/main/java/com/panlingxiao/shiro/helloworld/HelloWorld.java
@@ -1,12 +1,12 @@
 package com.panlingxiao.shiro.helloworld;
 
 import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.authc.IncorrectCredentialsException;
-import org.apache.shiro.authc.LockedAccountException;
-import org.apache.shiro.authc.UnknownAccountException;
-import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.authc.*;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.mgt.SecurityManager;
 import org.apache.shiro.session.Session;
 import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.Factory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,47 +19,83 @@ public class HelloWorld {
 
 
     public static void main(String[] args) {
+
+        //--------------构造Shiro的运行环境--------------------------
+
+        //从classpath中读取Ini配置文件，通过工厂创建Shrio的SecurityManger
+        Factory<SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro.ini");
+        SecurityManager securityManager = factory.getInstance();
+        //设置SecurityManager
+        SecurityUtils.setSecurityManager(securityManager);
+
+
+        // get the currently executing user:
+        // A Subject is just a security-specific "view" of an application User
+        // getSubject()根据当前线程或者请求获取到用户数据信息
         Subject currentUser = SecurityUtils.getSubject();
+
+
+
+        // Do some stuff with a Session (no need for a web or EJB container!!!)
+        /*
+         * 这里的Session是由Shiro所提供的,它和我们的HttpSession类似，但是并需要Http环境
+         */
         Session session = currentUser.getSession();
         session.setAttribute("someKey", "aValue");
 
-        if (!currentUser.isAuthenticated()) {
-            //collect user principals and credentials in a gui specific manner
-            //such as username/password html form, X509 certificate, OpenID, etc.
-            //We'll use the username/password example here since it is the most common.
-            //(do you know what movie this is from? ;)
-            UsernamePasswordToken token = new UsernamePasswordToken("lonestarr", "vespa");
-            //this is all you have to do to support 'remember me' (no config - built in!):
-            token.setRememberMe(true);
-            currentUser.login(token);
+        String value = (String) session.getAttribute("someKey");
+        if (value.equals("aValue")) {
+            log.info("Retrieved the correct value! [" + value + "]");
+        }
 
+        //----------------测试登入并且检查角色和权限---------------------
+
+        //判断用户是否认证
+        if (!currentUser.isAuthenticated()) {
+            UsernamePasswordToken token = new UsernamePasswordToken("lonestarr", "vespa");
+            token.setRememberMe(true);
             try {
                 currentUser.login(token);
-                //if no exception, that's it, we're done!
+                //输出用户信息
+                log.info("User [" + currentUser.getPrincipal() + "] logged in successfully.");
             } catch (UnknownAccountException uae) {
-                //username wasn't in the system, show them an error message?
+                log.info("There is no user with username of " + token.getPrincipal());
             } catch (IncorrectCredentialsException ice) {
-                //password didn't match, try again?
+                log.info("Password for account " + token.getPrincipal() + " was incorrect!");
             } catch (LockedAccountException lae) {
-                //account for that username is locked - can't login.  Show them a message?
+                log.info("The account for username " + token.getPrincipal() + " is locked.  " +
+                        "Please contact your administrator to unlock it.");
+            }
+            // 可以在这里捕获更多个用户自定义异常
+            catch (AuthenticationException ae) {
+                //unexpected condition?  error?
             }
 
-            //print their identifying principal (in this case, a username):
-            log.info("User [" + currentUser.getPrincipal() + "] logged in successfully.");
-
+            //test a role:
             if (currentUser.hasRole("schwartz")) {
                 log.info("May the Schwartz be with you!");
             } else {
                 log.info("Hello, mere mortal.");
             }
 
-            if (currentUser.isPermitted("winnebago:drive:eagle5")) {
-                log.info("You are permitted to 'drive' the 'winnebago' with license plate (id) 'eagle5'.  " +
+            //test a typed permission (not instance-level)
+            if (currentUser.isPermitted("lightsaber:abc")) {
+                log.info("You may use a lightsaber ring.  Use it wisely.");
+            } else {
+                log.info("Sorry, lightsaber rings are for schwartz masters only.");
+            }
+
+            //a (very powerful) Instance Level permission:
+            if (currentUser.isPermitted("user:get:5")) {
+                log.info("You are permitted to 'get' the user with (id) '5'.  " +
                         "Here are the keys - have fun!");
             } else {
                 log.info("Sorry, you aren't allowed to drive the 'eagle5' winnebago!");
             }
-            currentUser.logout(); //removes all identifying information and invalidates their session too.
+            //all done - log out!
+            currentUser.logout();
+
+            System.exit(0);
         }
     }
 }
---
+++ shrio-learning-helloworld/src/main/java/com/panlingxiao/shiro/helloworld/ShrioApp.java
@@ -0,0 +1,74 @@
+package com.panlingxiao.shiro.helloworld;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.UnknownAccountException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.session.Session;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.Factory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Arrays;
+
+/**
+ * Created by panlingxiao on 2016/9/2.
+ */
+public class ShrioApp {
+
+
+    private static final Logger log = LoggerFactory.getLogger(ShrioApp.class);
+
+    public static void main(String[] args) {
+        /*
+         * 构建 Shiro 环境
+         */
+        Factory<org.apache.shiro.mgt.SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro.ini");
+        org.apache.shiro.mgt.SecurityManager securityManager = factory.getInstance();
+        //设置SecurityManager
+        SecurityUtils.setSecurityManager(securityManager);
+
+        //获取当前用户
+        // A Subject is just a security-specific "view" of an application User.
+        Subject currentUser = SecurityUtils.getSubject();
+
+        Session session = currentUser.getSession();
+        session.setAttribute("hello", "吴大神V5");
+
+        //判断用户是否认证
+        if (!currentUser.isAuthenticated()) {
+            UsernamePasswordToken token = new UsernamePasswordToken("吴大神", "吴大神");
+            //用户登入
+            try {
+                currentUser.login(token);
+            } catch (UnknownAccountException e) {
+                log.info("用户名不存在");
+            } catch (IncorrectCredentialsException e) {
+                log.info("密码错误");
+            } catch (AuthenticationException e) {
+                e.printStackTrace();
+            }
+        }
+
+        //判断用户的角色
+        if(currentUser.hasAllRoles(Arrays.asList("superman","superdashen"))){
+            log.info("大神拥有的角色,大神V5");
+        }else{
+            log.info("大神没有所有的角色");
+        }
+        //判断权限
+        if(currentUser.isPermittedAll("meimei:delete:1","product:get:*","product:add","product:list","product:nimei")){
+            log.info("大神V5，拥有所有权限");
+        }
+
+        log.info("大神的Session,hello:{}",session.getAttribute("hello"));
+        currentUser.logout();
+
+
+       // log.info("大神的Session,hello:{}",session.getAttribute("hello"));
+
+    }
+}
